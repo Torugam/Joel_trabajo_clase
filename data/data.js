5bff9a3ab29171f56b3b0f33470b2faa9224ac1c
Joel <joelomarmena@gmail.com>
2024-05-15 21:14:21 -0600

    first commit

---
+++ data/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+      id: '1',
+      title: '#002752',
+      src: require('../src/img/azul.jpg'),
+    },
+    {
+      id: '2',
+      title: '#FF6600',
+      src: require('../src/img/anaranjado.png'),
+    },
+    {
+      id: '3',
+      title: '#7C7B79',
+      src: require('../src/img/gris.jpg'),
+    }
+  ];
+ 
+  export default data;
